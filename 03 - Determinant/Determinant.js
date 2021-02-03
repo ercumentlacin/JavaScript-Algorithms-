0bead79313750fa4c79a209790834ee461d4b14b
ercumenlacin <1140104023@nku.edu.tr>
2021-02-03 21:28:17 +0300

    function rename

--- 03 - Determinant/Determinant.js
+++ 03 - Determinant/Determinant.js
@@ -1,10 +1,13 @@
-const getDet = (a) => {
-  if (a.length === 1) return Array.isArray(a[0]) ? a[0][0] : a[0];
-  let d = 0;
-  for (let i = 0; i < a.length; i++)
-    d +=
-      a[0][i] *
-      getDet(a.slice(1).map((x) => x.filter((_, j) => i !== j))) *
+const getDet = (squareMatrix) => {
+  if (squareMatrix.length === 1)
+    return Array.isArray(squareMatrix[0])
+      ? squareMatrix[0][0]
+      : squareMatrix[0];
+  let determinant = 0;
+  for (let i = 0; i < squareMatrix.length; i++)
+    determinant +=
+      squareMatrix[0][i] *
+      getDet(squareMatrix.slice(1).map((x) => x.filter((_, j) => i !== j))) *
       (-1) ** i;
-  return d;
+  return determinant;
 };
