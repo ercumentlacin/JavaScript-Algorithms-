a791463442deadec8816084469cadd8808dbf6d2
ercumenlacin <1140104023@nku.edu.tr>
2021-02-04 18:08:02 +0300

    function operation(a, b, op) {

---
+++ 04 - BasicArithmeticOperations/BasicArithmeticOperations.js
@@ -0,0 +1,17 @@
+const operation = (a, b, op) => {
+  var num1 = parseInt(a);
+  var num2 = parseInt(b);
+  if (op === "add") {
+    return num1 + num2;
+  } else if (op === "divide") {
+    if (num2 === 0) {
+      return "undefined";
+    } else {
+      return num1 / num2;
+    }
+  } else if (op === "multiply") {
+    return num1 * num2;
+  } else if (op === "subtract") {
+    return num1 - num2;
+  }
+};
